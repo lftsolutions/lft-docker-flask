326422ffc4af8b7f2b3220ee673e0afda777b73b
kcrane <kcrane121@gmail.com>
2022-06-05 10:56:52 -0400

    clean up reference to app name

--- flasklate/routes.py
+++ flasklate/routes.py
@@ -3,5 +3,4 @@ from flask import render_template
 
 @app.route("/")
 def index():
-    project_name = app.root_path.rsplit('/',1)[1].strip()
-    return render_template('index.html', project_name=project_name)
+    return render_template('index.html', app_name=app.name)
