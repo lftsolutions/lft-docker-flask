29e47d3628aeff78da594bef69a4165017fd7153
Kyle <kcrane121@gmail.com>
2022-08-14 15:20:59 -0400

    initial logging setup configuration (#4)

--- flasklate/__init__.py
+++ flasklate/__init__.py
@@ -1,9 +1,30 @@
+import logging
 from flask import Flask
+from flask.logging import default_handler
+
+# Logger Creation
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# Log Formatter Creation
+formatter = logging.Formatter('%(levelname)s | %(asctime)s | %(process)d:%(thread)d | %(filename)s | %(funcName)s | %(lineno)d | %(message)s')
+
+# Log Stream Handler Creation
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+logger.addHandler(stream_handler)
 
 def create_app():
     app = Flask(__name__)
-    register_blueprints(app)
 
+    # Remove Default Flask Logger
+    app.logger.removeHandler(default_handler)
+
+    # Add Flask Blueprints
+    register_blueprints(app)
+    
+    logger.info("Starting App!")
+    
     return app
 
 
